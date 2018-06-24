bbabe0c4a08dd14a8aceaa0047a188a7065280ac
huangyb <757750514@qq.com>
2018-06-24 11:42:36 +0800

    add http utils

---
+++ utils/src/main/java/com/beyond/date/DateUtils.java
@@ -0,0 +1,48 @@
+package com.beyond.date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * @Auther: yanbing
+ * @Date: 2018/6/24 11:07
+ */
+public class DateUtils {
+
+    /**
+     * 根据当前系统时区把指定时间转换为UTC时间
+     *
+     * @param date:需要转换的时间
+     * @return
+     */
+    public static Long convertCurrentSystemZoneDateToUTC(Date date) {
+        Calendar cal = Calendar.getInstance();
+        //date为mull，则默认为当前系统时间
+        if (date != null) {
+            cal.setTime(date);
+        }
+
+        // 2、取得时间偏移量：
+        int zoneOffset = cal.get(java.util.Calendar.ZONE_OFFSET);
+        // 3、取得夏令时差：
+        int dstOffset = cal.get(java.util.Calendar.DST_OFFSET);
+        // 4、从本地时间里扣除这些差量，即可以取得UTC时间：
+        cal.add(java.util.Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+        return cal.getTimeInMillis();
+    }
+
+    /**
+     * 把UTC时间转换为当前系统时区时间
+     *
+     * @return
+     * @throws ParseException
+     */
+    public static Date getCurrentSystmZoneDateByUTC(String utc) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return sdf.parse(utc);
+    }
+}
---
+++ utils/src/main/java/com/beyond/http/HttpClientUtils.java
@@ -0,0 +1,116 @@
+package com.beyond.http;
+
+import com.beyond.character.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+/**
+ * Http Client工具包调用代码示例
+ * @Auther: yanbing
+ * @Date: 2018/6/24 11:07
+ */
+public class HttpClientUtils {
+    private static RequestConfig requestConfig;
+    private static final int MAX_TIMEOUT = 7000;
+    static {
+        RequestConfig.Builder configBuilder = RequestConfig.custom();
+        // 设置连接超时
+        configBuilder.setConnectTimeout(MAX_TIMEOUT);
+        // 设置读取超时
+        configBuilder.setSocketTimeout(MAX_TIMEOUT);
+        // 设置从连接池获取连接实例的超时
+        configBuilder.setConnectionRequestTimeout(MAX_TIMEOUT);
+        requestConfig = configBuilder.build();
+    }
+
+    /**
+     * 发送 SSL POST 请求（HTTPS），JSON形式
+     * @param apiUrl API接口URL
+     * @param json JSON对象
+     * @return
+     */
+    public static String doPostSSL(String apiUrl, Object json) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
+        CloseableHttpClient httpclient = createAcceptSelfSignedCertificateClient();
+        HttpPost httpPost = new HttpPost(apiUrl);
+        CloseableHttpResponse response = null;
+        String httpStr = null;
+        try {
+            httpPost.setConfig(requestConfig);
+            StringEntity stringEntity = new StringEntity(json.toString(),"UTF-8");//解决中文乱码问题
+            stringEntity.setContentEncoding("UTF-8");
+            stringEntity.setContentType("application/json");
+            httpPost.setEntity(stringEntity);
+            response = httpclient.execute(httpPost);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != HttpStatus.SC_OK) {
+                return null;
+            }
+            HttpEntity entity = response.getEntity();
+            if (entity == null) {
+                return null;
+            }
+            httpStr = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+        String result = StringUtils.decodeUnicode(httpStr);
+        return result;
+    }
+
+    private static CloseableHttpClient createAcceptSelfSignedCertificateClient()
+            throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[]{};
+            }
+
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+        } };
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        sslContext.init(null, trustAllCerts, new SecureRandom());
+        // use the TrustSelfSignedStrategy to allow Self Signed Certificates
+
+        // we can optionally disable hostname verification.
+        // if you don't want to further weaken the security, you don't have to include this.
+        HostnameVerifier allowAllHosts = new NoopHostnameVerifier();
+
+        // create an SSL Socket Factory to use the SSLContext with the trust self signed certificate strategy
+        // and allow all hosts verifier.
+        SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext, allowAllHosts);
+
+        // finally create the HttpClient using HttpClient factory methods and assign the ssl socket factory
+        return HttpClients
+                .custom()
+                .setSSLSocketFactory(connectionFactory)
+                .build();
+    }
+}
